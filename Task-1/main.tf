45b7a7a2b4f03c0a6f8a3710fbc4c8980d858226
Evgenii-Zuev <98023720+Evgenii-Zuev@users.noreply.github.com>
2022-03-22 19:34:40 +0400

    Update main.tf

--- Task-1/main.tf
+++ Task-1/main.tf
@@ -1,3 +1,7 @@
+#----------------------------------------------------------
+# Terraform
+# Output different wariables (for training)
+#----------------------------------------------------------
 terraform {
   required_providers {
     aws = {
@@ -6,39 +10,37 @@ terraform {
     }
   }
 }
-
 provider "aws" {
   region = "eu-central-1"
 }
-
 # Data ***********************************
-
+data "aws_availability_zones" "available" {}
 data "aws_vpcs" "allow-vpcs" {}
-
 data "aws_subnets" "allow-subnets" {
   filter {
     name   = "vpc-id"
     values = data.aws_vpcs.allow-vpcs.ids
   }
 }
-
 data "aws_security_groups" "allow-sg" {
   filter {
     name   = "vpc-id"
     values = data.aws_vpcs.allow-vpcs.ids
   }
 }
-
 # Outputs ********************************
-
+output "available_AZs_names" {
+  value = data.aws_availability_zones.available.names[*]
+}
+output "available_AZs_ids" {
+  value = data.aws_availability_zones.available.zone_ids[*]
+}
 output "allow-vpcs" {
   value = data.aws_vpcs.allow-vpcs.ids
 }
-
 output "allow-subnets" {
   value = data.aws_subnets.allow-subnets.ids
 }
-
 output "allow-sg" {
   value = data.aws_security_groups.allow-sg
 }
