6fe8803751fa90df5699b299cc6f5a18519d1c98
Evgenii Zuev <Evgenii_Zuev@epam.com>
2022-03-22 06:33:41 +0000

    new file:   Task-1/main.tf

---
+++ Task-1/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+# Data ***********************************
+
+data "aws_vpcs" "allow-vpcs" {}
+
+data "aws_subnets" "allow-subnets" {
+  filter {
+    name   = "vpc-id"
+    values = data.aws_vpcs.allow-vpcs.ids
+  }
+}
+
+data "aws_security_groups" "allow-sg" {
+  filter {
+    name   = "vpc-id"
+    values = data.aws_vpcs.allow-vpcs.ids
+  }
+}
+
+# Outputs ********************************
+
+output "allow-vpcs" {
+  value = data.aws_vpcs.allow-vpcs.ids
+}
+
+output "allow-subnets" {
+  value = data.aws_subnets.allow-subnets.ids
+}
+
+output "allow-sg" {
+  value = data.aws_security_groups.allow-sg
+}
